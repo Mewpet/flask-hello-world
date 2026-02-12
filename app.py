e4d9b9ef897df13c74d46da483d9508afa73e3e7
Mewpet <claire_gaddhayward@hotmail.co.uk>
2026-02-12 15:43:56 +0000

    added jeff

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! My name is OWO'
+    return """
+    <h1> Hi!!! Here's jeff! </h1>
+    <img src="https://giffiles.alphacoders.com/223/223280.gif" alt="Jeff">
+"""
